5465679859d0d014b1b19647e4552e50a0dd9526
Gersom <52709196+GersomR-afk@users.noreply.github.com>
2022-04-05 18:18:08 +0200

    Update javascript/axios-list.js
    Co-authored-by: Haylee Schäfer <mail@inventivetalent.org>

--- javascript/axios-list.js
+++ javascript/axios-list.js
@@ -4,10 +4,12 @@
 
 const axios = require('axios');
 
+const PAGE = 1;
+
 // send the request!
 axios.request({
     method: 'GET',
-    url: 'https://api.mineskin.org/get/list/0',
+    url: 'https://api.mineskin.org/get/list/' + PAGE,
 }).then(response => {
     // log the result
     console.log(response.data);
